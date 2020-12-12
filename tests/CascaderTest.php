36dfc7cc5ce86ccd01e95f7763ff41d65ddcd9c0
Alejandro Celaya <alejandro@alejandrocelaya.com>
2020-12-12 12:28:14 +0100

    Fixed CascaderTest compatibility with latest phpunit versions

--- tests/CascaderTest.php
+++ tests/CascaderTest.php
@@ -22,7 +22,7 @@ class CascaderTest extends TestCase
      */
     protected $cascader;
 
-    protected function setUp()
+    protected function setUp(): void
     {
         $this->cascader = new Cascader();
     }
